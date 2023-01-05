0e482685bb725fc7abb108455a48376b4080bb98
YebinKim <yb2316@naver.com>
2023-01-05 17:24:00 +0900

    init: Begin a Xcode Project

---
+++ ParrotBooks/ParrotBooks/Search/SearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchViewController.swift
+//  ParrotBooks
+//
+//  Created by vivi on 2023/01/05.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
