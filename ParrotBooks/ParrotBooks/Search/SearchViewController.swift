028c58f800f229ca90eef2d34cbcac72f6081c4f
YebinKim <yb2316@naver.com>
2023-01-06 08:11:16 +0900

    feat: [#4] SearchBar 추가 및 텍스트 입력 기능 추가

--- ParrotBooks/ParrotBooks/Search/SearchViewController.swift
+++ ParrotBooks/ParrotBooks/Search/SearchViewController.swift
@@ -7,9 +7,33 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+final class SearchViewController: UIViewController {
+    
+    private let searchBar = UISearchBar()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        setupSearchBar()
+    }
+    
+    private func setupSearchBar() {
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: searchBar)
+        searchBar.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 0)
+        searchBar.placeholder = "책 이름"
+        searchBar.delegate = self
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        #if DEBUG
+        print("[searchBar] text: \(searchBar.text ?? "no text")")
+        #endif
     }
 }
